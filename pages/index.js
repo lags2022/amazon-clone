c0444d10f3bd6f3ceb3aa5c71b8eb617efee6023
lags2022 <lguzman.58erb@outlook.com>
2023-04-14 13:03:01 -0500

    banner tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import Image from "next/image";
 import Head from "next/head";
 import Header from "@/components/Header";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
     <div>
-      <Head>
+      <Head className="bg-gray-100">
         <title>Amazon 2.0</title>
       </Head>
+
       <Header />
+
+      <main className=" max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* ProductFeed */}
+      </main>
     </div>
   );
 }
