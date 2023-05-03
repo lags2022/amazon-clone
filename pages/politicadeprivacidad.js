6a82f3a81947bddee6071aeec53b19cb3b2802b9
lags2022 <lguzman.58erb@outlook.com>
2023-05-03 12:31:45 -0500

    update extra

--- pages/politicadeprivacidad.js
+++ pages/politicadeprivacidad.js
@@ -49,12 +49,11 @@ export default function Politica() {
           <h3 className="font-bold">Log Data</h3>
           <p>
             I want to inform you that whenever you use my Service, in a case of
-            an error in the app I collect data and information (through
-            third-party products) on your phone called Log Data. This Log Data
-            may include information such as your device Internet Protocol (“IP”)
-            address, device name, operating system version, the configuration of
-            the app when utilizing my Service, the time and date of your use of
-            the Service, and other statistics.
+            an error in the app I collect data and information on your phone
+            called Log Data. This Log Data may include information such as your
+            device Internet Protocol address, device name, operating system
+            version, the configuration of the app when utilizing my Service, the
+            time and date of your use of the Service, and other statistics.
           </p>
         </div>
         <div className="pb-6 mx-8">
@@ -62,14 +61,13 @@ export default function Politica() {
           <p>
             Cookies are files with a small amount of data that are commonly used
             as anonymous unique identifiers. These are sent to your browser from
-            the websites that you visit and are stored on your device's internal
-            memory. This Service does not use these “cookies” explicitly.
-            However, the app may use third-party code and libraries that use
-            “cookies” to collect information and improve their services. You
-            have the option to either accept or refuse these cookies and know
-            when a cookie is being sent to your device. If you choose to refuse
-            our cookies, you may not be able to use some portions of this
-            Service.
+            the websites that you visit and are stored on your devices internal
+            memory. This Service does not use these cookies explicitly. However,
+            the app may use third-party code and libraries that use cookies to
+            collect information and improve their services. You have the option
+            to either accept or refuse these cookies and know when a cookie is
+            being sent to your device. If you choose to refuse our cookies, you
+            may not be able to use some portions of this Service.
           </p>
         </div>
         <div className="pb-6 mx-8">
@@ -80,10 +78,10 @@ export default function Politica() {
               following reasons:
             </p>
             <ul className="my-4 list-disc ml-4">
-              <li>To facilitate our Service;</li>
-              <li>To provide the Service on our behalf;</li>
-              <li>To perform Service-related services; or</li>
-              <li>To assist us in analyzing how our Service is used.</li>
+              <li>To facilitate our Service</li>
+              <li>To provide the Service on our behalf</li>
+              <li>To perform Service-related services or</li>
+              <li>To assist us in analyzing how our Service is used</li>
             </ul>
             <p>
               I want to inform users of this Service that these third parties
@@ -100,7 +98,7 @@ export default function Politica() {
             I value your trust in providing us your Personal Information, thus
             we are striving to use commercially acceptable means of protecting
             it. But remember that no method of transmission over the internet,
-            or method of electronic storage is 100% secure and reliable, and I
+            or method of electronic storage is secure and reliable, and I
             cannot guarantee its absolute security.
           </p>
         </div>
