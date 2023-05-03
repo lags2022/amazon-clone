65f3da5e372bcbd3c985c3eaeda6e1bf2e5aaf1d
lags2022 <lguzman.58erb@outlook.com>
2023-05-03 12:19:27 -0500

    update extra

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,29 +7,32 @@ import {
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { selectItems } from "@/redux/slices/basketSlice";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Header() {
   const { data: session } = useSession();
-  const router = useRouter();
   const items = useSelector(selectItems);
+  const router = useRouter();
 
   return (
     <header>
       {/* top nav */}
       <div className=" flex items-start bg-amazon_blue p-1 flex-grow py-2 ">
-        <div className=" mt-2 flex items-center flex-grow sm:flex-grow-0 ">
+        <Link
+          href="/"
+          className=" mt-2 flex items-center flex-grow sm:flex-grow-0 "
+        >
           <Image
-            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
             className="object-contain h-10 w-36 cursor-pointer"
             alt=""
           />
-        </div>
+        </Link>
 
         {/* search */}
         <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500 ">
@@ -42,24 +45,21 @@ function Header() {
 
         {/* right */}
         <div className=" text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div
+          <button
             onClick={!session ? signIn : signOut}
             className=" newcomponenttwd"
           >
-            <p className="hover:underline">
+            <p className="text-start">
               {session ? `Hello, ${session.user.name}` : "Sign In"}
             </p>
             <p className=" font-extrabold md:text-sm">Account & Lists</p>
-          </div>
-          <div
-            onClick={() => router.push("/orders")}
-            className=" newcomponenttwd"
-          >
+          </button>
+          <Link href="/orders" className=" newcomponenttwd">
             <p>Returns</p>
             <p className=" font-extrabold md:text-sm">& Orders</p>
-          </div>
-          <div
-            onClick={() => router.push("/checkout")}
+          </Link>
+          <Link
+            href="/checkout"
             className="relative newcomponenttwd flex items-center"
           >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
@@ -69,14 +69,16 @@ function Header() {
             <p className="hidden md:inline font-extrabold md:text-sm mt-2 ">
               Basket
             </p>
-          </div>
+          </Link>
         </div>
       </div>
 
       {/* bottom nav */}
       <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
         <div className=" newcomponenttwd flex items-center">
-          <FontAwesomeIcon icon={faBars} className="h-4 mr-2" />
+          <Link href="/politicadeprivacidad">
+            <FontAwesomeIcon icon={faBars} className="h-4 mr-2" />
+          </Link>
           <p>All</p>
         </div>
         <p className="newcomponenttwd ">Prime Video</p>
