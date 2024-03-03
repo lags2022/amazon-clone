dada4ad0e425c349844ce0ca74dece55282b47c6
lags2022 <lguzman.58erb@outlook.com>
2024-03-03 08:23:56 -0500

    feat: añdiendo cambios en la api stripe cors

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -2,43 +2,54 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function checkout(req, res) {
   try {
-    const { items, email } = req.body;
+    if (req.method === "POST") {
+      // Permitir solicitudes POST desde cualquier origen
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "POST");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    const transformedItems = items.map((item) => ({
-      quantity: 1,
-      price_data: {
-        currency: "usd",
-        unit_amount: item.price * 100,
-        product_data: {
-          name: item.title,
-          description: item.description,
-          images: [item.image],
+      console.log("BODY", req.body);
+      const { items, email } = req.body;
+
+      const transformedItems = items.map((item) => ({
+        quantity: 1,
+        price_data: {
+          currency: "usd",
+          unit_amount: item.price * 100,
+          product_data: {
+            name: item.title,
+            description: item.description,
+            images: [item.image],
+          },
         },
-      },
-    }));
+      }));
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      shipping_options: [
-        {
-          shipping_rate: "shr_1My1dOEfYxNbjr9ymfol5jXy",
+      console.log("no pasa");
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        shipping_options: [
+          {
+            shipping_rate: "shr_1My1dOEfYxNbjr9ymfol5jXy",
+          },
+        ],
+        shipping_address_collection: {
+          allowed_countries: ["GB", "US", "CA"],
         },
-      ],
-      shipping_address_collection: {
-        allowed_countries: ["GB", "US", "CA"],
-      },
-      line_items: transformedItems,
-      mode: "payment",
-      success_url: `${process.env.HOST}/success`,
-      cancel_url: `${process.env.HOST}/checkout`,
-      metadata: {
-        email,
-        images: JSON.stringify(items.map((item) => item.image)),
-      },
-    });
+        line_items: transformedItems,
+        mode: "payment",
+        success_url: `${process.env.HOST}/success`,
+        cancel_url: `${process.env.HOST}/checkout`,
+        metadata: {
+          email,
+          images: JSON.stringify(items.map((item) => item.image)),
+        },
+      });
 
-    res.status(200).json({ id: session.id });
+      res.status(200).json({ id: session.id });
+    }
   } catch (error) {
+    console.log("FUCKING ERROR", error.message);
     res.status(400).json({ error: error.message });
   }
 }
